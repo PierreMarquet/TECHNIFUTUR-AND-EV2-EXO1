9b53745541d8406a7eeaef335f5fbb54c23b24cd
Pierre Marquet <pierre.marquet@outlook.be>
2021-07-02 11:59:59 +0200

    third commit

--- app/src/main/java/be/technifutur/exo1/TwitterActivity.kt
+++ app/src/main/java/be/technifutur/exo1/TwitterActivity.kt
@@ -3,36 +3,44 @@ package be.technifutur.exo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 
 class TwitterActivity : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_twitter)
 
+       //binding = ActivityMainBinding.inflate(layoutInflater)
+       //setContentView(binding.root)
+
+        connexion()
 
     }
 
 
-    fun connexion(view: View){
+    private fun connexion() {
 
-        val loginOK = findViewById(R.id.twitterTextEmailAddress) as EditText
+        val loginOK = findViewById<EditText>(R.id.twitterTextEmailAddress)
 
         if(loginOK.text.isNotEmpty()){
-            connexionVerification(view)
+            connexionVerification()
+
         }else{
-            alertMessage(view)
+            alertMessage()
+
         }
     }
 
-    fun connexionVerification(view: View){
+    fun connexionVerification() {
         val intent = Intent(this, TwitterConnexion::class.java)
         startActivity(intent)
     }
 
-    fun alertMessage(view: View){
+    private fun alertMessage() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setMessage("Login incomplet")
             .setTitle("Erreur")
